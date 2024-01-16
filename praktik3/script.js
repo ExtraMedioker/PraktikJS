3f1a2f6a27fe9701b14b040ce1b1a902033b4523
ExtraMedioker <mzidanabdulkadir@gmail.com>
2024-01-16 15:13:20 +0800

    praktik ketiga

---
+++ praktik3/script.js
@@ -0,0 +1,7 @@
+let namaNegara = "Angola";
+const benua = "Africa";
+let populasi = 35981281;
+const kepulauan = false;
+let bahasa = "Portugis";
+
+console.log(`Negara ${namaNegara} adalah negara yang berada di benua ${benua}, dengan populasi sebanyak ${populasi} juta jiwa, bahasa nasionalnya adalah ${bahasa}`);
